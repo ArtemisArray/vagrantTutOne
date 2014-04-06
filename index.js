15a6ec784d5e17b906a7eb8f80ab7d504c5b737b
ArtemisArray <anncolorist@gmail.com>
2014-04-06 13:20:57 -0700

    updating gitignore

--- index.js
+++ index.js
@@ -10,3 +10,29 @@ var server = app.listen(3000, function() {
 });
 
 console.log("hello world");
+
+ var MongoClient = require('mongodb').MongoClient
+    , format = require('util').format;
+
+  MongoClient.connect('mongodb://127.0.0.1:27017/test', function(err, db) {
+	    //console.log("connected to db");
+	    if(err) throw err;
+
+	    var collection = db.collection('test_insert');
+	    collection.insert({a:2}, function(err, docs) {
+	    	console.log("the error is ", err);
+	    	console.log("the docs are ", docs);
+
+	      	collection.count(function(err, count) {
+	      		console.log(format("count = %s", count));
+	     	});
+
+	    //   // Locate all the entries using find
+	       collection.find().toArray(function(err, results) {
+	        console.dir(results);
+	    //     // Let's close the db
+	         db.close();
+	       });
+	     });
+	    console.log("Hello2");
+  })
